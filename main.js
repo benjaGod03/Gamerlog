258505a04846a70a052fdd5c839dbe7df089f903
Cande Fernández <2004candefernandez@gmail.com>
2025-10-08 21:55:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function abrirModalLogin() {
+  document.getElementById('modalLogin').classList.remove('oculto');
+}
+
+// Cerrar el modal perfil
+function cerrarModalLogin() {
+  document.getElementById('modalLogin').classList.add('oculto');
+}
