8499282b89361afbfad1316729c2a7c9151d8acf
benja <puentebbenjamin@gmail.com>
2025-10-09 19:32:49 -0300

    modal registro y boton login

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-function abrirModalLogin() {
-  document.getElementById('modalLogin').classList.remove('oculto');
-}
+//cerrar y abrir modal registro
+document.getElementById('openModal').onclick = function(e) {
+  e.preventDefault();
+  document.getElementById('modal').style.display = 'block';
+};
+document.getElementById('closeModal').onclick = function() {
+  document.getElementById('modal').style.display = 'none';
+};
+window.onclick = function(event) {
+  if (event.target == document.getElementById('modal')) {
+    document.getElementById('modal').style.display = 'none';
+  }
+};
 
-// Cerrar el modal perfil
-function cerrarModalLogin() {
-  document.getElementById('modalLogin').classList.add('oculto');
-}
+//mostrar login
+document.getElementById('showLogin').onclick = function(e) {
+    e.preventDefault();
+    document.getElementById('showLogin').style.display = 'none';
+    document.getElementById('openModal').style.display = 'none';
+    document.getElementById('loginForm').style.display = 'flex';
+};
+document.getElementById('cancelLogin').onclick = function(e) {
+    e.preventDefault();
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('showLogin').style.display = 'inline-block';
+    document.getElementById('openModal').style.display = 'inline-block';
+};
